a2f5ee8b5f37505b46a4228264dc45d2f7d51614
Surya <koroth.surya@gmail.com>
2025-11-30 13:50:24 +0530

    secondcommit

--- src/gittest/Newgit.java
+++ src/gittest/Newgit.java
@@ -5,6 +5,7 @@ public class Newgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
+System.out.println("git update testing");
 }
 
 }
